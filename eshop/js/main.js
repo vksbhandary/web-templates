b38b2ade23e6ef75ef1c754c80252e0e592c0c87
Vikas Bhandary <vksbhandary@gmail.com>
2021-06-16 17:44:45 +0545

    added buttons to scroll product list

--- eshop/js/main.js
+++ eshop/js/main.js
@@ -26,3 +26,50 @@ $('.allcategory_container').scroll(function () {
 });
 
 initActiveMenu($('.allcategory_container'));
+
+function getCurrentScroll(element){
+  var firstprod= $(element).find('.product_single').first();
+  var current_scroll = $(element).offset().left - firstprod.offset().left;
+  return current_scroll;
+}
+function getScrollWidth(element){
+  var lastprod = $(element).find('.product_single').last();
+  var maxcroll = lastprod.offset().left + lastprod.width() - $(element).width() - $(element).offset().left;
+  return maxcroll;
+}
+$('.product_slider').on('scroll',function(){
+  var element = $(this);
+  var current_scroll = getCurrentScroll(element), maxcroll = getScrollWidth(element);
+  var leftbutton = $(this).parent().find('.slide-button.left');
+  var rightbutton = $(this).parent().find('.slide-button.right');
+  if (current_scroll > 0){
+    leftbutton.css('visibility','visible');
+  }else{
+    leftbutton.css('visibility','hidden');
+  }
+  if (maxcroll > 0){
+    rightbutton.css('visibility','visible');
+  }else{
+    rightbutton.css('visibility','hidden');
+  }
+});
+
+$('.slide-button.left').on('click',function(){
+  var slider =$(this).parent().find('.product_slider');
+  var current_scroll = getCurrentScroll(slider), maxcroll = getScrollWidth(slider);
+  if (current_scroll>0){
+    slider.animate({
+      scrollLeft: current_scroll-350
+  }, 1000);
+  }
+});
+
+$('.slide-button.right').on('click',function(){
+  var slider =$(this).parent().find('.product_slider');
+  var current_scroll = getCurrentScroll(slider), maxcroll = getScrollWidth(slider);
+  if (maxcroll>0){
+    slider.animate({
+      scrollLeft: current_scroll+350
+  }, 1000);
+  }
+});
